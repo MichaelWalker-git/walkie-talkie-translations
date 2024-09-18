dfa6cc3cf8abba34ac6cf291e7dc7c1ac83bd1b0
michaelwalker <michaelhuytran@gmail.com>
2024-09-17 21:30:31 -0700

    fix: dependencies issues

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,17 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'translation-walkie-talkie',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    'dotenv',
+    '@aws-cdk/aws-appsync-alpha',
+    '@aws-sdk/client-transcribe',
+    'axios',
+    '@aws-sdk/client-polly',
+    'node-fetch',
+    '@aws-sdk/client-translate',
+    '@aws-sdk/client-s3',
+    '@aws-sdk/client-sfn',
+  ], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
